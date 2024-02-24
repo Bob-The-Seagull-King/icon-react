1dd78ef9402d83af2f69b4b7433c95e99e21811c
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-02-25 10:03:21 +1300

    JobDisplay and Data Update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,13 +6,13 @@ import AddonDisplay from '../components/AddonDisplay'
 import AbilityDisplay from '../components/AbilityDisplay'
 import TraitsDisplay from '../components/TraitsDisplay'
 import LimitBreakDisplay from '../components/LimitBreakDisplay'
+import JobDisplay from '../components/JobDisplay'
 
 import summonData from '../resources/data/summon.json';
 import relicData from '../resources/data/relic.json';
 import addonData from '../resources/data/abilityaddon.json';
 import abilityData from '../resources/data/ability.json';
 import classData from '../resources/data/class.json';
-
 
 const HomePage: React.FC = () => {
 
@@ -37,7 +37,7 @@ const HomePage: React.FC = () => {
         <div style={{ position: 'relative', paddingLeft: '30%', paddingRight: '30%',  width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <h1 style={{ fontSize: '4em' }}>Hello world!</h1>
             {jobArray.map((item) => (
-                    <LimitBreakDisplay key={item.name} data={item}/>
+                    <JobDisplay key={item.name} data={item}/>
                     ))}
         </div>
     )
