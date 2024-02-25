f9949fd55a339c99ae989a62f58c3b1840176d2f
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-02-26 12:22:49 +1300

    Relic Style

--- src/components/RelicDisplay.tsx
+++ src/components/RelicDisplay.tsx
@@ -1,6 +1,7 @@
 import moment from 'moment'
 import React, { useEffect, useState } from 'react'
 import { capitalizeTag, getColour } from '../utility/functions';
+import '../styles/iconcomponent.scss';
 
 const RelicDisplay = (props: any) => {
     /**
@@ -19,20 +20,20 @@ const RelicDisplay = (props: any) => {
 
     // Return render -------------------------------
     return (
-        <div>
-            <h1 style={{color: getColour(relicData.colour)}}>{capitalizeTag( relicData.name )}</h1>
+        <div className='relicStructure'>
+            <h1 className={'titleShape title'+getColour(relicData.colour)}>{capitalizeTag( relicData.name )}</h1>
             <p><i>{relicData.desc}</i></p>
             {tier >= 1 &&
-                <p>I.   <span dangerouslySetInnerHTML={{__html: (relicData.tier1 || '')}}></span></p>
+                <p><b>I.</b>   <span dangerouslySetInnerHTML={{__html: (relicData.tier1 || '')}}></span></p>
             }
             {tier >= 2 &&
-                <p>II.  <span dangerouslySetInnerHTML={{__html: (relicData.tier2 || '')}}></span></p>
+                <p><b>II.</b>  <span dangerouslySetInnerHTML={{__html: (relicData.tier2 || '')}}></span></p>
             }
             {tier >= 3 &&
-                <p>III. <span dangerouslySetInnerHTML={{__html: (relicData.tier3 || '')}}></span></p>
+                <p><b>III.</b> <span dangerouslySetInnerHTML={{__html: (relicData.tier3 || '')}}></span></p>
             }
             {tier >= 4 &&
-                <p><b>Aspected: </b> <span dangerouslySetInnerHTML={{__html: (relicData.tier4 || '')}}></span></p>
+                <p><b>Aspected:</b> <span dangerouslySetInnerHTML={{__html: (relicData.tier4 || '')}}></span></p>
             }
             <p><i>Aspect quest: </i> <span dangerouslySetInnerHTML={{__html: (relicData.quest || '')}}></span></p>
         </div>
