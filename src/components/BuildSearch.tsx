a3c9e624483ead9b64bdc5411eb656bb12def8b2
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-02-29 10:24:46 +1300

    SearchBuild Basic Layout

--- src/components/BuildSearch.tsx
+++ src/components/BuildSearch.tsx
@@ -14,18 +14,60 @@ const BuildSearch = (props: any) => {
         <div className='basesearchStructure'>
             <div className='searchContainer'>
                 <div className='searchgriditem'> 
-                    <div className='basesearchitemStructure'>
-                        <h2 className='centerPosition'>TEST</h2>
+                    <div className='searchSubContainer'>
+                        <div className='basesearchitemStructure jobgriditem'>
+                            <div className='centerPosition'>
+                                <h2 className='paddedSearchTitle'>JOB</h2>
+                                <input className='searchinput'/>
+                            </div>
+                        </div>
+                        <br/>
+                        <div className='searchsubsubcontainer'>
+                            <div className='basesearchitemStructure levelgriditem'>
+                            <div className='centerPosition'>
+                                <h2 className='paddedSearchLevel'>LV</h2>
+                                <input className='searchinput'/>
+                            </div>
+                            </div>
+                            <div className='basesearchitemStructure searchgridbutton'>
+                                <h2 className='centerPosition'>&#x1F50E;&#xFE0E;</h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='searchgriditem'> 
-                    <div className='basesearchitemStructure'>
-                        <h2 className='centerPosition'>TEST</h2>
+                    <div className='searchSubContainer'>
+                        <div className='basesearchitemStructure searchgriditem'>
+                            <div className='centerPosition'>
+                                <h2 className='paddedSearchLevel'>RELIC</h2>
+                                <input className='searchinputrelic'/>
+                                <input className='searchinputreliclevel'/>
+                                <div className='paddedSearchAdd'>
+                                    <h2 className='nakedpad'>ADD</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <br/>
+                        <div className='searchitemscontainer'>
+                        </div>
                     </div>
                 </div>
-                <div className='searchgriditem'> 
-                    <div className='basesearchitemStructure'>
-                        <h2 className='centerPosition'>TEST</h2>
+                <div className='searchgriditem'>  
+                    <div className='searchSubContainer'>
+                        <div className='basesearchitemStructure searchgriditem'>
+                        <div className='centerPosition'>
+                                <h2 className='paddedSearchLevel'>ABILITY</h2>
+                                <input className='searchinputrelic'/>
+                                <input className='searchinputreliclevel'/>
+                                <input className='searchinputabilitymastery'/>
+                                <div className='paddedSearchAdd'>
+                                    <h2 className='nakedpad'>ADD</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <br/>
+                        <div className='searchitemscontainer'>
+                        </div>
                     </div>
                 </div>
             </div>
