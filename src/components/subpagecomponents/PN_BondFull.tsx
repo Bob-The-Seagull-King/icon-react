e9d8c19d7ad9e3ad138d950ea353b72574cba2dd
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-03-07 15:00:07 +1300

    extra missed page creation

---
+++ src/components/subpagecomponents/PN_BondFull.tsx
@@ -0,0 +1,37 @@
+import moment from 'moment'
+import React, { useEffect, useState } from 'react'
+import { capitalizeTag, getColour } from '../../utility/functions';
+import '../../styles/iconcomponent.scss';
+
+const PN_BondFull = (props: any) => {
+    
+    const searchVal = urlParse();
+    const isSearched = (value: any) => (true);
+
+    function urlParse() {
+        const urlPath = window.location.pathname;
+        const urlSplits = urlPath.split('/');
+        if (urlSplits.length >= 5) {
+            
+            urlSplits[4] = urlSplits[4].replaceAll('%20', ' ');
+            return urlSplits[4];
+        }
+        return "";
+    }
+
+    // Navigation ----------------------------------
+    function navClick (dir: string, name: string) {    
+        window.open(location.protocol + '//' + location.host + '/player/tactics/' + dir + '/' + name, '_blank', 'noopener,noreferrer');
+    }
+    // ---------------------------------------------
+
+
+    // Return render -------------------------------
+    return (
+        <div>
+            Player_Narrative_Bond_Full
+        </div>
+    )
+}
+
+export default PN_BondFull
