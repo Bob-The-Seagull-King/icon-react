51f4fbef6a023f368452531d61acb985ae2bfc1f
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-03-07 14:59:11 +1300

    Reorg

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ROUTES } from './resources/routes-constants'
-import TacticsPlayerPage from './pages/TacticsPlayerPage'
+import TacticsPlayerPage from './pages/PlayerTacticalPage'
 import BuildPage from './pages/BuildPage'
 import HomePage from './pages/HomePage'
 import './styles/main.sass'
