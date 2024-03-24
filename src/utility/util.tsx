5a96919acbef32485d1098a9777805dae189058c
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-03-25 10:07:07 +1300

    Summon popups

--- src/utility/util.tsx
+++ src/utility/util.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import GlossaryPopup from "../components/pagecomponents/GlossaryPopup";
+import SummonPopup from '../components/pagecomponents/SummonPopup';
 
 export function convertStringToContent(contentstring: string) {
-    const split = contentstring.split(/({{{{[^}]{0,}}}}})/g);
+    const split = contentstring.split(/(\[\[\[\[[^\]]*\]\]\]\])|({{{{[^}]{0,}}}}})/g);
+    console.log(split);
     return (
         <span>
-            {split.map((item) => (
+            {split.filter(item => item !== undefined).map((item) => (
                 <span key={item + ( Math.random().toString())}>{checkerStringToContent(item)}</span>
             ))}
         </span>
@@ -17,6 +19,10 @@ function checkerStringToContent(string: string) {
         if ((string.substring(0,4) == "{{{{")) {
             return (
                 <GlossaryPopup data={string.substring(4,string.length-4)} />
+            )
+        } else if ((string.substring(0,4) == "[[[[")) {
+            return (
+                <SummonPopup data={string.substring(4,string.length-4)} />
             )
         } else {
             return (
