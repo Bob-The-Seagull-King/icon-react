ac1f6db164f453b52a279ffcac36a744d9456b30
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-03-31 17:59:03 +1300

    fix2

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ROUTES } from './resources/routes-constants'
 
-import './resources/styles/_icon'
+import './resources/styles/_icon.scss'
 
 const RootComponent: React.FC = () => {
     return (
