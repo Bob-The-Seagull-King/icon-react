6a9c362c904668d441775793a2af079db39fc920
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-04-02 19:35:56 +1300

    col fix 1.1

--- src/display/superroutes/HomeRoute.tsx
+++ src/display/superroutes/HomeRoute.tsx
@@ -15,7 +15,7 @@ const HomeRoute: React.FC = () => {
     // Return result -----------------------------
     return (
         <div className="container">
-            <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 row-cols-xs-1 row-cols-1">
+            <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 row-cols-1">
                 {AbilitiesCollectionController.ReturnAbilities().map((item) => (
                         <div className="col" key={"abilityDisplay"+item.ID}>
                             <AbilityDisplay data={item}/>
