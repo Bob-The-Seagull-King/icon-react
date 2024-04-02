8a18ba452bd3aa93cc8467d1354f5e140e172246
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-04-03 12:41:29 +1300

    abilities structure without filter bar

--- src/display/pages/PlayerTacticsAbilities.tsx
+++ src/display/pages/PlayerTacticsAbilities.tsx
@@ -33,22 +33,41 @@ const PlayerTacticsAbilities = (prop: any) => {
     // Return result -----------------------------
     return (
         <div className="container">
-            <div className="row my-0 py-0">
-                {_foundItems.map((item) => (
-                        <div className="col-12 my-0 py-0" key={"tableItemDisplay"+item.HeldItem.ID}>
-                            <ViewTableItemDisplay data={item} parent={AbilitiesCollectionController} statefunction={ItemRecall}/>
-                        </div>
-                    ))}
+            <div className="row">
+                <br/>
             </div>
-                <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 row-cols-1">
-                    {_activeItems.map((item) => (
+            <div className="row">
+                <div className="col-lg-5 col-md-6 col-sm-12 col-xs-12 col-12 my-0 py-0">
+                    <div className="row">
+                        <div className='col-12'>
+                            <br/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        {_foundItems.map((item) => (
+                            <div className="col-12 my-0 py-0" key={"tableItemDisplay"+item.HeldItem.ID}>
+                                <ViewTableItemDisplay data={item} parent={AbilitiesCollectionController} statefunction={ItemRecall}/>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="col-lg-7 col-md-6 col-sm-12 col-xs-12 col-12">
+                    <div className="row">
+                        <div className='col-12'>
+                            <br/>
+                        </div>
+                    </div>
+                    <div className="row row-cols-lg-2 row-cols-md-1 row-cols-sx-1 row-cols-xs-1 row-cols-1">
+                        {_activeItems.map((item) => (
                             <div className="col" key={"abilityDisplay"+item.ID}>
                                 <AbilityDisplay data={item}/>
                                 <br/>
                             </div>
                         ))}
+                    </div>
                 </div>
             </div>
+        </div>
     )
     // -------------------------------------------
 }
