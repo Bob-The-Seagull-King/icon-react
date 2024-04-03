36706ad74345359901a650feec1843aa41c6c2e2
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-04-04 12:03:45 +1300

    filters are now objects

--- src/classes/viewmodel/collections/filters/AbilitiesFilterManager.ts
+++ src/classes/viewmodel/collections/filters/AbilitiesFilterManager.ts
@@ -1,23 +1,23 @@
 import { IPlayerAbility, PlayerAbility } from "../../../../classes/feature/abilities/Ability";
 import { AbilityFactory } from "../../../../factories/features/AbilityFactory";
 import { getColour } from "../../../../utility/functions";
-import { IFilterItem, IFilterObject, IFilterTag, IFilterText } from "./FilterInterfaces"
+import { IFilterItem, IFilterObject, IFilterTag, IFilterText, FilterTag, FilterItem, FilterText } from "./FilterInterfaces"
 import { Requester } from "../../../../factories/Requester";
 
 class AbilitiesFilterManager {
-    TextOptions: IFilterText[];
-    TagOptions: IFilterTag[];
-    MiscOptions: IFilterObject[];
+    TextOptions: FilterText[];
+    TagOptions: FilterTag[];
+    MiscOptions: FilterItem[];
 
     constructor() {
-        this.TextOptions = [{group: "name", val: "", isstrict: false}]
+        this.TextOptions = [new FilterText({group: "name", val: "", isstrict: false})]
         this.TagOptions = this.FindTags();
         this.MiscOptions = this.FindMisc();
     }
 
     FindTags() {
-        const tempTags: IFilterTag[] = []
-        const foundTags = Requester.MakeRequest({ searchtype: 'tags', searchparam: { type: 'abilities' } });
+        const tempTags: FilterTag[] = []
+        const foundTags = (Requester.MakeRequest({ searchtype: 'tags', searchparam: { type: 'abilities' } })).sort();
 
         let i = 0;
         for (i = 0; i < foundTags.length; i++) {
@@ -28,25 +28,27 @@ class AbilitiesFilterManager {
                                                     tagtype: tempTagObject,
                                                     tagval: tempTagText
                                                     }
-
-            tempTags.push(tempTagInterface);
+            const tempTagConstructed = new FilterTag(tempTagInterface);
+            tempTags.push(tempTagConstructed);
         }
 
         return tempTags;
     }
 
     FindMisc() {
-        const tempMisc: IFilterObject[] = []
+        const tempMisc: FilterItem[] = []
         const keytypes = ["source", "chapter", "class_id", "job_id"]
+        keytypes.sort();
 
         let i = 0;
         for (i = 0; i < keytypes.length; i ++) {
-            const foundVals = Requester.MakeRequest({ searchtype: 'keyvalues', searchparam: { type: 'abilities' , id: keytypes[i]} });
+            const foundVals = Requester.MakeRequest({ searchtype: 'keyvalues', searchparam: { type: 'abilities' , id: keytypes[i]} }).sort();
             
             let j = 0;
             for (j = 0; j < foundVals.length; j++) {
-                const tempTagObject: IFilterItem = { group: keytypes[i], isactive: false, doinclude: false, name: foundVals[j]}
-                tempMisc.push(tempTagObject);
+                const tempItemObject: IFilterItem = { group: keytypes[i], isactive: false, doinclude: false, name: foundVals[j]}
+                const tempItemConstructed = new FilterItem(tempItemObject);
+                tempMisc.push(tempItemConstructed);
             }
         }
 
--- src/classes/viewmodel/collections/filters/FilterInterfaces.ts
+++ src/classes/viewmodel/collections/filters/FilterInterfaces.ts
@@ -18,4 +18,42 @@ interface IFilterTag extends IFilterObject {
     tagval: IFilterText;
 }
 
-export {IFilterObject, IFilterText, IFilterItem, IFilterTag}
+class FilterText {
+    Group: string;
+    Val: string;
+    IsStrict: boolean;
+
+    constructor(item: IFilterText) {
+        this.Group = item.group;
+        this.Val = item.val;
+        this.IsStrict = item.isstrict;
+    }
+}
+
+class FilterItem {
+    Group: string;
+    IsActive: boolean;
+    DoInclude: boolean;
+    Name: string;
+
+    constructor(item: IFilterItem) {
+        this.Group = item.group;
+        this.IsActive = item.isactive;
+        this.DoInclude = item.doinclude;
+        this.Name = item.name;
+    }
+}
+
+class FilterTag {
+    Group: string;
+    TagType: FilterItem;
+    TagVal: FilterText;
+
+    constructor(item: IFilterTag) {
+        this.Group = item.group;
+        this.TagType = new FilterItem(item.tagtype);
+        this.TagVal = new FilterText(item.tagval);
+    }
+}
+
+export {IFilterObject, IFilterText, IFilterItem, IFilterTag, FilterText, FilterItem, FilterTag}
