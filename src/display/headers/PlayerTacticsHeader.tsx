444229cec28d327057961f377a61066a45d8b5f2
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-04-15 17:12:55 +1200

    abilitiespage header fin

--- src/display/headers/PlayerTacticsHeader.tsx
+++ src/display/headers/PlayerTacticsHeader.tsx
@@ -2,8 +2,16 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../../resources/styles/_icon.scss'
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+
 
 const PlayerTacticsHeader = (prop: any) => {
+
+    function NavigateHome() {
+        window.open(location.protocol + '//' + location.host +'/', '_self', 'noopener,noreferrer');
+    }
 
     // Return result -----------------------------
     return (
@@ -12,7 +20,9 @@ const PlayerTacticsHeader = (prop: any) => {
             <h1 className='headertext'>ICONPENDIUM</h1>
             <p className='headersubtext'>v1.6.01a</p>
             <div className="vr headertext"></div>
-            <Button variant="">Primary</Button>{' '}
+            <Button style={{padding:"0em"}} variant="" onClick={() => NavigateHome()}>
+                <FontAwesomeIcon icon={faHouse} style={{fontSize:"2em",color:"white",margin:"0em"}}/>
+            </Button>
             <div className="vr headertext"></div>
             <div className="ms-auto headertext"></div>
         </div>
