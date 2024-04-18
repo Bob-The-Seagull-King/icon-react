57ffa21b88b416ac7d45fcab70310a845c420a9e
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-04-18 13:14:04 +1200

    variable width content pack

--- src/display/components/features/contentpack/ContentPackDisplay.tsx
+++ src/display/components/features/contentpack/ContentPackDisplay.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../../../../resources/styles/_icon.scss'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
@@ -27,71 +27,138 @@ const ContentPackDisplay = (props: any) => {
         parentView.DeletePack(PackItem);
         updateHost();
     }
+    
+    const [stateWidth, setWidth] = useState(0);
+    const ref = useRef<HTMLDivElement>(null);
+    
+    useEffect(() => {
+        const setContentPackWidth = () => {
+            if(ref.current) {
+                setWidth(ref.current.clientWidth);
+            }
+            console.log(stateWidth)
+        }
+        window.addEventListener("load", setContentPackWidth, false);
+        window.addEventListener("resize", setContentPackWidth, false);
+        setContentPackWidth();
+        console.log(stateWidth)
+    }, [stateWidth])
 
     return (
         <>
-            <div className='contentpackcontainer smallbordersubpurple'>
-                <span className="packvrbox">
-                    <Button style={{padding:"0em"}} variant="" onClick={() => handleShow()}>
-                        <FontAwesomeIcon icon={faBookOpen} className="purpleIcon" style={{fontSize:"2em",margin:"0em"}}/>
-                    </Button>
-                    <div className="vr packvr"></div>
-                    <Button style={{padding:"0em"}} variant="" onClick={() => handleClose()}>
-                        {PackItem.isactive &&
-                            <FontAwesomeIcon icon={faUnlock} className="greenIcon" style={{fontSize:"2em",margin:"0em"}}/>
-                        }
-                        {!PackItem.isactive &&
-                            <FontAwesomeIcon icon={faLock} className="redIcon" style={{fontSize:"2em",margin:"0em"}}/>
-                        }
-                    </Button>
-                    <div className="vr packvr"></div>
-                </span>
-
-                <h1 className="packtitle">
-                    {PackItem.name}
-                </h1>
-                <div className="vr packvr"></div>
-                <h3 className="packsubtitle">
-                    {PackItem.author}
-                </h3>
-
-                <span className="packvrbox">
-                    <div className="vr packvr"></div>
-                    <Button style={{padding:"0em"}} variant="" onClick={() => removeContentPack()}>
-                        <FontAwesomeIcon icon={faTrash} className="redIcon" style={{fontSize:"2em",margin:"0em"}}/>
-                    </Button>
-                </span>
-            </div>
-            <Modal size="lg" onEnterKeyDown={() => handleClose()} show={show}  contentClassName="filterboxStructure" dialogClassName="" onHide={handleClose} keyboard={true}  centered>
-                
-                            <h1 className={'titleShape titlepurple'}>
+            <div className='' ref={ref}>
+                {stateWidth > 700 &&
+                    <div className="contentpackcontainer smallbordersubpurple" >
+                        <span className="packvrbox">
+                            <Button style={{padding:"0em"}} variant="" onClick={() => handleShow()}>
+                                <FontAwesomeIcon icon={faBookOpen} className="purpleIcon" style={{fontSize:"2em",margin:"0em"}}/>
+                            </Button>
+                            <div className="vr packvr"></div>
+                            <Button style={{padding:"0em"}} variant="" onClick={() => handleClose()}>
+                                {PackItem.isactive &&
+                                    <FontAwesomeIcon icon={faUnlock} className="greenIcon" style={{fontSize:"2em",margin:"0em"}}/>
+                                }
+                                {!PackItem.isactive &&
+                                    <FontAwesomeIcon icon={faLock} className="redIcon" style={{fontSize:"2em",margin:"0em"}}/>
+                                }
+                            </Button>
+                            <div className="vr packvr"></div>
+                        </span>
+                        <span className="contentsubnamecontainer">
+                            <span/>
+                            <h1 className="packtitle">
                                 {PackItem.name}
-                                <div className="row float-end">
-                                    <div className='col-12 float-end'>
-                                        <Button style={{padding:"0em"}} variant="" onClick={() => handleClose()}>
-                                            <FontAwesomeIcon icon={faCircleXmark} style={{fontSize:"2em",color:"white",margin:"0em"}}/>
-                                        </Button>
-                                    </div>
-                                </div>
                             </h1>
-                            <Modal.Body >
-                                <div className="row overflow-auto flex-grow-1 m-0 p-0">
-                                    <div style={{"maxHeight": "calc(70vh"}}>
-                                        <div className="separator" style={{marginTop:"0em"}}><h5>By {PackItem.author}</h5></div>
-                                        <div className="col-12" style={{fontSize:"0.95em"}}>
-                                            {PackItem.description}
-                                        </div>
-                                        <div className="separator" style={{marginTop:"0em"}}><h5>Content</h5></div>
-                                        <div style={{display:"flex",flexWrap:"wrap"}}>
-                                            {PackItem.tags.map((item: any) => (
-                                                <div className="filterobjectdisplay" key={"packdisplay"}>
-                                                    {makestringpresentable(item.name)} - {item.count.toString()}
-                                                </div>
-                                            ))}
-                                        </div>
+                            <div className="vr packvr"></div>
+                            <h3 className="packsubtitle">
+                                {PackItem.author}
+                            </h3>
+                            <span/>
+                        </span>
+                        <span className="packvrbox">
+                            <div className="vr packvr"></div>
+                            <Button style={{padding:"0em"}} variant="" onClick={() => removeContentPack()}>
+                                <FontAwesomeIcon icon={faTrash} className="redIcon" style={{fontSize:"2em",margin:"0em"}}/>
+                            </Button>
+                        </span>
+                    </div>
+                }
+                {stateWidth <= 700 &&
+                    <div className="contentpacksmallcontainer smallbordersubpurple" >
+                        
+                        <div className="row" style={{width:"100%"}}>
+                            <div className="col-12 smallcontentpackrow" style={{display: "flex", justifyContent:"space-between"}}>
+                                <span/>
+                                <h1 className="packtitle" style={{width:"fit-content"}}>
+                                    {PackItem.name}
+                                </h1>
+                                <span/>
+                            </div>
+                        </div>
+                        <div className="row" style={{width:"100%"}}>
+                            <div className="col-12 smallcontentpackrow" style={{display: "flex", justifyContent:"space-between"}}>
+                                <span/>
+                                <h3 className="packsubtitle" style={{width:"fit-content"}}>
+                                    {PackItem.author}
+                                </h3>
+                                <span/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 smallcontentpackrow" style={{display: "flex", justifyContent:"space-between"}}>
+                                <span/>
+                                <Button style={{padding:"0em"}} variant="" onClick={() => handleShow()}>
+                                    <FontAwesomeIcon icon={faBookOpen} className="purpleIcon" style={{fontSize:"2em",margin:"0em"}}/>
+                                </Button>
+                                <div className="vr packvr"></div>
+                                <Button style={{padding:"0em"}} variant="" onClick={() => handleClose()}>
+                                    {PackItem.isactive &&
+                                        <FontAwesomeIcon icon={faUnlock} className="greenIcon" style={{fontSize:"2em",margin:"0em"}}/>
+                                    }
+                                    {!PackItem.isactive &&
+                                        <FontAwesomeIcon icon={faLock} className="redIcon" style={{fontSize:"2em",margin:"0em"}}/>
+                                    }
+                                </Button>
+                                <div className="vr packvr"></div>
+                                <Button style={{padding:"0em"}} variant="" onClick={() => removeContentPack()}>
+                                    <FontAwesomeIcon icon={faTrash} className="redIcon" style={{fontSize:"2em",margin:"0em"}}/>
+                                </Button>
+                                <span/>
+                            </div>
+                        </div>
+                    </div>
+                }
+            </div>
+
+            <Modal size="lg" onEnterKeyDown={() => handleClose()} show={show}  contentClassName="filterboxStructure" dialogClassName="" onHide={handleClose} keyboard={true}  centered>    
+                <h1 className={'titleShape titlepurple'}>
+                    {PackItem.name}
+                    <div className="row float-end">
+                        <div className='col-12 float-end'>
+                            <Button style={{padding:"0em"}} variant="" onClick={() => handleClose()}>
+                                <FontAwesomeIcon icon={faCircleXmark} style={{fontSize:"2em",color:"white",margin:"0em"}}/>
+                            </Button>
+                        </div>
+                    </div>
+                </h1>
+                <Modal.Body >
+                    <div className="row overflow-auto flex-grow-1 m-0 p-0">
+                        <div style={{"maxHeight": "calc(70vh"}}>
+                            <div className="separator" style={{marginTop:"0em"}}><h5>By {PackItem.author}</h5></div>
+                            <div className="col-12" style={{fontSize:"0.95em"}}>
+                                {PackItem.description}
+                            </div>
+                            <div className="separator" style={{marginTop:"0em"}}><h5>Content</h5></div>
+                            <div style={{display:"flex",flexWrap:"wrap"}}>
+                                {PackItem.tags.map((item: any) => (
+                                    <div className="filterobjectdisplay" key={"packdisplay"}>
+                                        {makestringpresentable(item.name)} - {item.count.toString()}
                                     </div>
-                                </div>
-                            </Modal.Body>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </Modal.Body>
             </Modal>
         </>
     )
