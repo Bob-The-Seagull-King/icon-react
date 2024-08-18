a55bffb7fbd4d9c86b3bdc9c8e5a7f2483c5c64a
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-18 13:20:54 +1200

    Structure Changes

--- src/display/components/features/jobs/JobDisplay.tsx
+++ src/display/components/features/jobs/JobDisplay.tsx
@@ -14,6 +14,7 @@ import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 import GenericDisplay from '../../../components/generics/GenericDisplay';
 import TraitDisplay from '../trait/TraitDisplay';
 import LimitBreakDisplay from '../abilities/LimitBreakDisplay';
+import AddonDisplay from '../addons/AddonDisplay';
 
 const JobDisplay = (props: any) => {
     const JobObject: Job = props.data
@@ -27,9 +28,11 @@ const JobDisplay = (props: any) => {
             <div className="verticalspacer"/>
             <div className="row row-cols-lg-2 row-cols-md-2 row-cols-sm-1">
                 <div className="col">
+                    <div className="verticalspacer"/>
                     {returnDescription(JobObject, JobObject.Blurb)} 
                 </div>
                 <div className="col">
+                    <div className="verticalspacer"/>
                     {returnDescription(JobObject, JobObject.Playstyle)} 
                 </div>
             </div>
@@ -38,23 +41,32 @@ const JobDisplay = (props: any) => {
                 <div className="separator">&#x27E1;</div>
             </div> 
             <div className="verticalspacer"/>
+            <div className="row row-cols-lg-2 row-cols-md-2 row-cols-sm-1">
+                <div className="col">
+                    <div className="verticalspacer"/>
+                    {JobObject.Traits.map((item) => <GenericDisplay key="traitloop" d_colour={JobObject.Class} d_name={item.Name} d_type={"sub"} d_method={() => <TraitDisplay data={item} />}/>)}             
+                </div>
+                <div className="col">
+                    <div className="verticalspacer"/>
+                    <GenericDisplay key="traitloop" d_colour={JobObject.Class} d_name={JobObject.LimitBreak.Name} d_type={"sub"} d_method={() => <LimitBreakDisplay data={JobObject.LimitBreak} colour={JobObject.Class}/>}/>
+                </div>
+            </div>
+            {JobObject.Addons.length > 0 &&
             <div>
+                <div className="verticalspacer"/> 
+                <div>
+                    <div className="separator">&#x27E1;</div>
+                </div> 
+                <div className="verticalspacer"/>
                 <div className="row">
                     <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 768: 2}} >
                         <Masonry gutter="20px">
-                            {JobObject.Traits.map((item) => <GenericDisplay key="traitloop" d_colour={JobObject.Class} d_name={item.Name} d_type={"sub"} d_method={() => <TraitDisplay data={item} />}/>)}             
+                            {JobObject.Addons.map((item) => <GenericDisplay key="addonloop" d_colour={JobObject.Class} d_name={item.Name} d_type={"sub"} d_method={() => <AddonDisplay data={item} />}/>)}
                         </Masonry>
                     </ResponsiveMasonry>
                 </div>
             </div>
-            <div className="verticalspacer"/> 
-            <div>
-                <div className="separator">Limit Break</div>
-            </div> 
-            <div className="verticalspacer"/>
-            <div>
-                <GenericDisplay key="traitloop" d_colour={JobObject.Class} d_name={JobObject.LimitBreak.Name} d_type={"sub"} d_method={() => <LimitBreakDisplay data={JobObject.LimitBreak} colour={JobObject.Class}/>}/>
-            </div>
+            }
         </div>
     )
 }
