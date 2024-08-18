edd98339ba49fdd5deb4faaa026de42726c5a6e0
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-18 15:31:39 +1200

    basic class structure

---
+++ src/classes/feature/class/Class.ts
@@ -0,0 +1,90 @@
+import { IIconpendiumItemData, IconpendiumItem, ItemType } from '../../IconpendiumItem'
+import { DescriptionFactory } from '../../../utility/functions';
+import { Trait } from '../trait/Trait';
+import { TraitFactory } from '../../../factories/features/TraitFactory';
+import { IRule } from '../rule/Rule';
+import { Job } from '../jobs/Job';
+import { JobFactory } from '../../../factories/features/JobFactory';
+import { RuleFactory } from '../../../factories/features/RuleFactory';
+
+interface IClass extends IIconpendiumItemData {
+    subtitle: string,
+    description: [], // Mechanical description of the item
+    traits: string[],
+    jobs: string[],
+    specialmechanic: IRule,
+    gambit: IRule,
+    complexity: string,
+    strengths: string,
+    weaknessess: string,
+    stats: IStats
+}
+
+interface IStats {
+    vit: number,
+    hp : number,
+    defense : number,
+    speed : number,
+    dash : number,
+    fray : number,
+    damage : number,
+    basicrange : number
+}
+
+class Class extends IconpendiumItem {
+    public readonly Subtitle;
+    public readonly Description;
+    public readonly Traits;
+    public readonly Jobs;
+    public readonly SpecialMechanic;
+    public readonly Gambit;
+    public readonly Complexity;
+    public readonly Strength;
+    public readonly Weakness;
+    public readonly Stats;
+
+    /**
+     * Assigns parameters and creates a series of description
+     * objects with DescriptionFactory
+     * @param data Object data in IPlayerAbility format
+     */
+    public constructor(data: IClass)
+    {
+        super(data)
+        this.Subtitle = data.subtitle;
+
+        this.Description = DescriptionFactory(data.description);
+        this.Traits = this.TraitsFactory(data.traits);
+        this.Jobs = this.JobsFactory(data.jobs)
+
+        this.SpecialMechanic = RuleFactory.CreateRule(data.specialmechanic);
+        this.Gambit = RuleFactory.CreateRule(data.gambit);
+
+        this.Complexity = data.complexity;
+        this.Strength = data.strengths;
+        this.Weakness = data.weaknessess;
+        this.Stats = data.stats;
+    }
+
+    private TraitsFactory(_data : string[]) {
+        const array : Trait[] = []
+        let i = 0;
+        for (i = 0; i < _data.length; i++) {
+            array.push(TraitFactory.CreateNewTrait(_data[i], 'traits'))
+        }
+        return array;
+    }
+
+    private JobsFactory(_data : string[]) {
+        const array : Job[] = []
+        let i = 0;
+        for (i = 0; i < _data.length; i++) {
+            array.push(JobFactory.CreateNewJob(_data[i]))
+        }
+        return array;
+    }
+
+}
+
+export {IClass, Class, IStats}
+
--- src/factories/features/JobFactory.ts
+++ src/factories/features/JobFactory.ts
@@ -1,3 +1,4 @@
+import { Requester } from '../Requester';
 import { IJob, Job } from '../../classes/feature/jobs/Job'
 
 class JobFactory {
@@ -11,6 +12,13 @@ class JobFactory {
         const ability = new Job(_ability)
         return ability;
     }  
+
+    static CreateNewJob(_val : string) {
+        const jobdata = Requester.MakeRequest({searchtype: "id", searchparam: {type: "jobs", id: _val}}) as IJob
+        const jobNew = JobFactory.CreateJob(jobdata)
+        return jobNew;
+    }   
+
 
 }
 
