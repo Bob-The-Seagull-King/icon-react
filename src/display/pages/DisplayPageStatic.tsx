2434e3228dca589b4f17f8ccfe0b054ae15c0717
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-18 16:20:52 +1200

    display static for classes

--- src/display/pages/DisplayPageStatic.tsx
+++ src/display/pages/DisplayPageStatic.tsx
@@ -5,6 +5,7 @@ import GenericDisplay from "../components/generics/GenericDisplay"
 import React from 'react'
 import SummonDisplay from "../components/features/summons/SummonDisplay"
 import JobDisplay from "../components/features/jobs/JobDisplay"
+import ClassDisplay from "../components/features/classes/ClassDisplay"
 
 export interface DisplayCollectionType {
     searchId      : string,
@@ -198,6 +199,55 @@ export const DisplayCollectionDataDex : DisplayCollectionDataTable = {
                             <div className='filterbox centerPosition'>
                                 {manager.ReturnMiscFilters().filter((value) => (value.Group == "class_id")).map((item) => (
                                     <FilterMiscItem key={"misclass"+item.Name} data={item} />
+                                ))}
+                            </div>
+                        </div>
+                            
+                        <div className='separator toppad'></div>
+                        <div className="row float-end">
+                            <div className='col-12 float-end'>
+                                <div className='hovermouse filterclosebutton' onClick={() => {close()}}>CONFIRM</div>
+                            </div>
+                        </div>
+                    </div>
+               </>
+            )
+        }
+    },
+    classes: {
+        searchId: 'classes',
+        width: 9,
+        returnDisplay(item: any) {
+            return (
+                <GenericDisplay  d_colour={item.Class} d_name={item.Name} d_type={""} d_method={() => <ClassDisplay data={item} />}/>
+            )
+        },
+        returnFilterSelect(manager : FilterManager, update : NoneToNoneFunction, close : NoneToNoneFunction) {
+            return (
+                <>
+                    <div className="col-12">
+                        <div className="separator"><h3>NAME</h3></div>
+                        <div className="row">
+                            {manager.ReturnTextFilters().map((item) => (
+                                <FilterTextItem data={item} key="name"/>
+                            ))}
+                        </div>
+                        <div className="separator"><h3>TAGS</h3></div>
+                        <div className="subltenotetext">{"You can specify tag's value in the text box. Leave blank to find all of that tag."}
+                        </div>
+                        <div className='toppad'></div>
+                        <div className="row">
+                            <div className="filterbox centerPosition">
+                                {manager.ReturnTagFilters().map((item) => (
+                                    <FilterTagItem key={"tag"+item.TagType.Name} data={item}/>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="separator"><h3>SOURCES</h3></div>
+                        <div className="row">
+                            <div className='filterbox centerPosition'>
+                                {manager.ReturnMiscFilters().filter((value) => (value.Group == "source")).map((item) => (
+                                    <FilterMiscItem key={"miscsource"+item.Name} data={item} />
                                 ))}
                             </div>
                         </div>
