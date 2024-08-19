7881e0d1b6d06d74630ed49faa5459e34572aa47
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-19 15:06:33 +1200

    relic display update

--- src/display/components/features/relics/RelicDisplay.tsx
+++ src/display/components/features/relics/RelicDisplay.tsx
@@ -3,22 +3,67 @@ import '../../../../resources/styles/_icon.scss'
 import React from 'react'
 
 // Classes
-import { returnTags, returnDescription } from '../../../../utility/util';
-import {PlayerAbility } from "../../../../classes/feature/abilities/Ability";
-import { Trait } from '../../../../classes/feature/trait/Trait';
-import { TraitFactory } from '../../../../factories/features/TraitFactory';
-import { Relic } from '../../../../classes/feature/relics/Relic';
+import { returnDescription } from '../../../../utility/util';
+import { Relic, RelicAction } from '../../../../classes/feature/relics/Relic';
 
 // Components
-import GenericDisplay from '../../../components/generics/GenericDisplay';
-import TraitDisplay from '../trait/TraitDisplay';
+import { makestringpresentable } from '../../../../utility/functions';
 
 const RelicDisplay = (props: any) => {
     const RelicObject: Relic = props.data
-    const bannedAbilityTags = ["inflict", "type"]
+
+    function returnRelicAction(_title : string, _item : RelicAction) {
+        let title = _title;
+
+        if (_item.Invoke === true) {
+            if (_item.InvokeType) {
+                title += "(Invoke : " + makestringpresentable( _item.InvokeType)
+                if (_item.InvokeVal) {
+                    title += " " + _item.InvokeVal
+                }
+                title += ")"
+            }
+        }
+
+        return (
+            <span>{title + ": "} {returnDescription(_item, _item.Description)}</span>
+        )
+    }
 
     return (
         <div className='abilityInternalStructure'>
+            <div>
+                {returnDescription(RelicObject, RelicObject.Blurb)}
+            </div> 
+            <div className="verticalspacer"/> 
+            <div>
+                <div className="separator">&#x27E1;</div>
+            </div> 
+            <div className="verticalspacer"/>
+
+            <div>
+                {returnRelicAction("Tier I", RelicObject.TierI)}
+            </div>
+            <div>
+                {returnRelicAction("Tier II", RelicObject.TierII)}
+            </div>
+            <div>
+                {returnRelicAction("Tier III", RelicObject.TierIII)}
+            </div>
+
+            <div className="verticalspacer"/> 
+            <div>
+                <div className="separator">Aspect</div>
+            </div> 
+            <div className="verticalspacer"/>
+
+            <div>
+                {returnRelicAction("Aspected", RelicObject.Aspected)}
+            </div>
+            
+            <div>
+                {returnDescription(RelicObject, RelicObject.Quest)}
+            </div> 
             
         </div>
     )
