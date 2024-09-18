a7e48023d387242fe921e2048e184fd082f4dcd6
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-09-18 21:53:42 +1200

    Trait Display update 18/09/2024 21:53

--- src/display/components/features/trait/TraitDisplay.tsx
+++ src/display/components/features/trait/TraitDisplay.tsx
@@ -3,13 +3,17 @@ import '../../../../resources/styles/_icon.scss'
 import React from 'react'
 
 // Classes
-import { returnDescription } from '../../../../utility/util';
+import { returnDescription, returnTags } from '../../../../utility/util';
 import { Trait } from '../../../../classes/feature/trait/Trait';
 
 const TraitDisplay = (props: any) => {
     const TraitObject: Trait = props.data
     return (
         <div className='abilityInternalStructure'>
+            <div>
+                {returnTags(TraitObject.Tags, [])}
+            </div>
+            <div className="verticalspacer"/>
             <div>
                 {returnDescription(TraitObject, TraitObject.Description)}
             </div>
